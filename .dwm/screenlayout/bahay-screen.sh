7f52da4cffe859d165f2164980b5bd202c746045
nopan-studio <caneda.s.bscs@gmail.com>
2022-12-26 19:27:09 +0800

    fix push

--- .dwm/screenlayout/bahay-screen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-xrandr --output eDP --primary --mode 1920x1080 --left-of HDMI-A-0 --rotate normal --output HDMI-A-0 --mode 1920x1080
--- .dwm/screenlayout/office-screen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-xrandr --output eDP --off --output HDMI-A-0 --mode 1920x1080
-q
